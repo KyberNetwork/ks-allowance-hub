fd30b78c8795c788b23046b91423d9552014b3bd
Vu Nguyen Hoang <nghoangvu24@gmail.com>
2025-12-15 11:02:48 +0700

    deploy: deploy test version on base and arbitrum

---
+++ script/DeployKSAllowanceHub.s.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import 'ks-common-sc/script/Base.s.sol';
+import 'src/KSAllowanceHub.sol';
+
+contract DeployKSAllowanceHubScript is BaseScript {
+  string salt = '251215';
+
+  function run(string[] memory chainIds) public multiChain(chainIds) {
+    address admin = _readAddress('admin');
+    address[] memory guardians = _readAddressArray('guardians');
+    address[] memory rescuers = _readAddressArray('rescuers');
+    address permit2 = _readAddress('permit2');
+
+    bytes memory creationCode = abi.encodePacked(
+      type(KSAllowanceHub).creationCode, abi.encode(admin, guardians, rescuers, permit2)
+    );
+
+    address allowanceHub =
+      _create3Deploy(keccak256(bytes(string.concat('KSAllowanceHub_', salt))), creationCode);
+    _writeAddress('allowance-hub', allowanceHub);
+  }
+}
